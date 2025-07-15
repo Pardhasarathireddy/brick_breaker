c52f2b4c9c97e768a0828578e85debf404f297c2
Pardhasarathireddy <pardhasarathireddy9@gmail.com>
2025-07-15 12:20:57 +0530

    Re-deploy Flutter web build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2317161358"
+    serviceWorkerVersion: "2029752900"
   }
 });
